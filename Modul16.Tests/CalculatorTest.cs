2b944d9fb2fd7b5274696c2696f957ab63b9ee08
Dmitrij Sokolov <DmitrijVS@gmail.com>
2022-08-25 17:13:51 +0300

    Добавьте файлы проекта.

---
+++ Modul16.Tests/CalculatorTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul16.Tests
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(30, 10) == 40);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(2, 3) == 6);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(20, 4) == 5);
+        }
+
+        [Test]
+        public void DivisionMustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+
+    }
+}
